0d426104da0b2a01bf404d5ed7d2a654f7e4c231
mikemonson <mike.03@me.com>
2016-12-03 18:30:13 -0800

    Updated drect
    rectangle can now resize and move on canvas

--- src/CS151/DRect.java
+++ src/CS151/DRect.java
@@ -1,34 +1,212 @@
 package CS151;
 
+import java.util.Arrays;
+import javafx.geometry.Point2D;
+import javafx.scene.Cursor;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
-public class DRect extends DShape implements ModelListener { 
+public class DRect extends DShape implements ModelListener
+{
 
-    private Rectangle rectangle;
+    private Rectangle rect;
+    private final double handleWidth = 8;
+    private final double handleCenter = handleWidth / 2;
 
-    
-    public DRect() {
-	model = new DRectModel();
-	rectangle = new Rectangle();
+    public DRect()
+    {
+
+        model = new DRectModel();
+        rect = new Rectangle(model.getX(), model.getY(), model.getWidth(), model.getHeight());
+        rect.setFill(model.getColor());
+        // top left resize handle:
+        Rectangle resizeHandleNW = new Rectangle(handleWidth, handleWidth, Color.GOLD);
+        // bind to top left corner of Rectangle:
+        resizeHandleNW.xProperty().bind(rect.xProperty().subtract(handleCenter));
+        resizeHandleNW.yProperty().bind(rect.yProperty().subtract(handleCenter));
+
+        // top right resize handle:
+        Rectangle resizeHandleNE = new Rectangle(handleWidth, handleWidth, Color.BLACK);
+        // bind to top right corner of Rectangle:
+        resizeHandleNE.xProperty().bind(rect.xProperty().add(rect.widthProperty().subtract(handleCenter)));
+        resizeHandleNE.yProperty().bind(rect.yProperty().subtract(handleCenter));
+
+        // bottom right resize handle:
+        Rectangle resizeHandleSE = new Rectangle(handleWidth, handleWidth, Color.BLACK);
+        // bind to bottom right corner of Rectangle:
+        resizeHandleSE.xProperty().bind(rect.xProperty().add(rect.widthProperty().subtract(handleCenter)));
+        resizeHandleSE.yProperty().bind(rect.yProperty().add(rect.heightProperty().subtract(handleCenter)));
+
+        // bottom left resize handle:
+        Rectangle resizeHandleSW = new Rectangle(handleWidth, handleWidth, Color.BLACK);
+        resizeHandleSW.xProperty().bind(rect.xProperty().subtract(handleCenter));
+        resizeHandleSW.yProperty().bind(rect.yProperty().add(rect.heightProperty().subtract(handleCenter)));
+
+        
+
+        // force circles to live in same parent as rectangle:
+        rect.parentProperty().addListener((obs, oldParent, newParent) -> {
+            for (Rectangle r : Arrays.asList(resizeHandleNW, resizeHandleSE, resizeHandleNE, resizeHandleSW)) {
+                Pane currentParent = (Pane) r.getParent();
+                if (currentParent != null) {
+                    currentParent.getChildren().remove(r);
+                }
+                ((Pane) newParent).getChildren().add(r);
+            }
+        });
+
+        Wrapper<Point2D> mouseLocation = new Wrapper<>();
+
+        setUpDragging(resizeHandleNW, mouseLocation);
+        setUpDragging(resizeHandleSE, mouseLocation);
+        setUpDragging(rect, mouseLocation);
+        setUpDragging(resizeHandleNE, mouseLocation);
+        setUpDragging(resizeHandleSW, mouseLocation);
+
+        resizeHandleNW.setOnMouseDragged(event -> {
+            if (mouseLocation.value != null) {
+                double deltaX = event.getSceneX() - mouseLocation.value.getX();
+                double deltaY = event.getSceneY() - mouseLocation.value.getY();
+                double newX = rect.getX() + deltaX;
+                if (newX >= handleWidth
+                        && newX <= rect.getX() + rect.getWidth() - handleWidth) {
+                    rect.setX(newX);
+                    rect.setWidth(rect.getWidth() - deltaX);
+                }
+                double newY = rect.getY() + deltaY;
+                if (newY >= handleWidth
+                        && newY <= rect.getY() + rect.getHeight() - handleWidth) {
+                    rect.setY(newY);
+                    rect.setHeight(rect.getHeight() - deltaY);
+                }
+                mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
+            }
+        });
+
+        resizeHandleNE.setOnMouseDragged(event -> {
+
+            if (mouseLocation.value != null) {
+                double deltaX = event.getSceneX() - mouseLocation.value.getX();
+                double deltaY = event.getSceneY() - mouseLocation.value.getY();
+                double newMaxX = rect.getX() + rect.getWidth() + deltaX;
+
+                if (newMaxX >= rect.getX()
+                        && newMaxX <= rect.getParent().getBoundsInLocal().getWidth() - handleWidth) {
+                    rect.setWidth(rect.getWidth() + deltaX);
+                }
+
+                double newY = rect.getY() + deltaY;
+
+                if (newY >= handleWidth
+                        && newY <= rect.getY() + rect.getHeight() - handleWidth) {
+                    rect.setY(newY);
+                    rect.setHeight(rect.getHeight() - deltaY);
+                }
+
+                mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
+            }
+        });
+
+        resizeHandleSE.setOnMouseDragged(event -> {
+            if (mouseLocation.value != null) {
+                double deltaX = event.getSceneX() - mouseLocation.value.getX();
+                double deltaY = event.getSceneY() - mouseLocation.value.getY();
+                double newMaxX = rect.getX() + rect.getWidth() + deltaX;
+                if (newMaxX >= rect.getX()
+                        && newMaxX <= rect.getParent().getBoundsInLocal().getWidth() - handleWidth) {
+                    rect.setWidth(rect.getWidth() + deltaX);
+                }
+                double newMaxY = rect.getY() + rect.getHeight() + deltaY;
+                if (newMaxY >= rect.getY()
+                        && newMaxY <= rect.getParent().getBoundsInLocal().getHeight() - handleWidth) {
+                    rect.setHeight(rect.getHeight() + deltaY);
+                }
+                mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
+            }
+        });
+
+        resizeHandleSW.setOnMouseDragged(event -> {
+            if (mouseLocation.value != null) {
+
+                double deltaX = event.getSceneX() - mouseLocation.value.getX();
+                double deltaY = event.getSceneY() - mouseLocation.value.getY();
+                double newX = rect.getX() + deltaX;
+                if (newX >= handleWidth
+                        && newX <= rect.getX() + rect.getWidth() - handleWidth) {
+                    rect.setX(newX);
+                    rect.setWidth(rect.getWidth() - deltaX);
+                }
+                double newMaxY = rect.getY() + rect.getHeight() + deltaY;
+                if (newMaxY >= rect.getY()
+                        && newMaxY <= rect.getParent().getBoundsInLocal().getHeight() - handleWidth) {
+                    rect.setHeight(rect.getHeight() + deltaY);
+                }
+                mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
+            }
+
+        });
+
+        rect.setOnMouseDragged(event -> {
+            if (mouseLocation.value != null) {
+                double deltaX = event.getSceneX() - mouseLocation.value.getX();
+                double deltaY = event.getSceneY() - mouseLocation.value.getY();
+                double newX = rect.getX() + deltaX;
+                double newMaxX = newX + rect.getWidth();
+                if (newX >= handleWidth
+                        && newMaxX <= rect.getParent().getBoundsInLocal().getWidth() - handleWidth) {
+                    rect.setX(newX);
+                }
+                double newY = rect.getY() + deltaY;
+                double newMaxY = newY + rect.getHeight();
+                if (newY >= handleWidth
+                        && newMaxY <= rect.getParent().getBoundsInLocal().getHeight() - handleWidth) {
+                    rect.setY(newY);
+                }
+                mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
+            }
+
+        });
+
     }
 
-    
+    private void setUpDragging(Rectangle r, Wrapper<Point2D> mouseLocation)
+    {
+
+        r.setOnDragDetected(event -> {
+            r.getParent().setCursor(Cursor.CLOSED_HAND);
+            mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
+        });
+
+        r.setOnMouseReleased(event -> {
+            r.getParent().setCursor(Cursor.DEFAULT);
+            mouseLocation.value = null;
+        });
+    }
+
+    static class Wrapper<T>
+    {
+
+        T value;
+    }
+
     /**
      * Draw the rectangle defined by the model
      */
-    public void draw(){
-	rectangle.setFill(model.getColor());
-	rectangle.setX((double) model.getX());
-	rectangle.setY((double) model.getY());
-	rectangle.setWidth((double) model.getWidth());
-	rectangle.setHeight((double) model.getHeight());	
+    public void draw()
+    {
+        rect.setFill(model.getColor());
+        rect.setX((double) model.getX());
+        rect.setY((double) model.getY());
+        rect.setWidth((double) model.getWidth());
+        rect.setHeight((double) model.getHeight());
     }
 
-    
-    public Shape getShape() {
-	return rectangle;
+    public Shape getShape()
+    {
+        return rect;
     }
-    
+
 }
--- src/CS151/DShape.java
+++ src/CS151/DShape.java
@@ -2,68 +2,87 @@ package CS151;
 
 import javafx.scene.shape.Shape;
 import javafx.scene.paint.Color;
-import java.awt.Rectangle;
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Arrays;
+import javafx.geometry.Point2D;
+import javafx.scene.Cursor;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
-public abstract class DShape implements ModelListener {
+public abstract class DShape implements ModelListener
+{
 
     protected DShapeModel model;
-    
-    DShape() {
-	model = new DShapeModel();
-    }
-	
-    public DShapeModel getModel() {
-	return model;
+
+    DShape()
+    {
+        model = new DShapeModel();
     }
 
-    public void setModel(DShapeModel model) {
-	this.model = model;
-    }
-    
-    public Rectangle getBounds() {
-	return model.getBounds();
-    }
-    
-    public void randomize(int max) {
-	model.randomize(max);
+    public DShapeModel getModel()
+    {
+        return model;
     }
 
-    public void setColor(Color c) {
-	model.setColor(c);
+    public void setModel(DShapeModel model)
+    {
+        this.model = model;
     }
 
+    public java.awt.Rectangle getBounds()
+    {
+        return model.getBounds();
+    }
+
+    public void randomize(int max)
+    {
+        model.randomize(max);
+    }
+
+    public void setColor(Color c)
+    {
+        model.setColor(c);
+    }
+    
+    
 
     /**
-     * Calculate a list of points representing
-     * the four corners of the model
-     * @return ArrayList<Point> 
+     * Calculate a list of points representing the four corners of the model
+     *
+     * @return ArrayList<Point>
      */
-    public ArrayList<Point> getKnobs() {
-	ArrayList<Point> knob = new ArrayList<Point>();
-	knob.add(model.getTopLeft());
-	knob.add(model.getTopRight());
-	knob.add(model.getBottomLeft());
-	knob.add(model.getBottomRight());
-	return knob;
+    public ArrayList<Point> getKnobs()
+    {
+        ArrayList<Point> knob = new ArrayList<Point>();
+        knob.add(model.getTopLeft());
+        knob.add(model.getTopRight());
+        knob.add(model.getBottomLeft());
+        knob.add(model.getBottomRight());
+        return knob;
     }
-    
-    public void drawKnobs() {
 
-    }
-    
-    public void removeKnobs() {
+    public void drawKnobs()
+    {
 
     }
 
-    public void modelChanged(DShapeModel model) {
-	setModel(model); // set the new changes
-	draw(); // redraw
+    public void removeKnobs()
+    {
+
     }
-    
+
+    public void modelChanged(DShapeModel model)
+    {
+        setModel(model); // set the new changes
+        draw(); // redraw
+    }
+
     public abstract void draw();
+
     public abstract Shape getShape();
-    
+
+
 
 }
